7332cd97e2597786d8301faeb0a6e0f04f782a2d
pseudorandom <7276677+pseudorandomhk@users.noreply.github.com>
2023-11-13 13:46:40 -0800

    Make state fields available as StateModifiers (TEMP FIX, wait for next RandomizerCore release)

---
+++ RandomizerMod/RC/StateVariables/StateFieldModifierVariables.cs
@@ -0,0 +1,62 @@
+﻿using RandomizerCore.Logic;
+using RandomizerCore.Logic.StateLogic;
+
+namespace RandomizerMod.RC.StateVariables
+{
+    internal class StateFieldModifierVariable : StateModifier
+    {
+        private static Dictionary<string, string> fieldModifierNames = new()
+        {
+            { "$NOFLOAT", "NOFLOAT" },
+            { "$FLOAT", "FLOAT" },
+            { "$PFLOAT", "PFLOAT" },
+            { "$DIVEFLOAT", "DIVEFLOAT" },
+            { "$DIVEWALKOUT", "DIVEWALKOUT" }
+        };
+
+        public override string Name { get; }
+
+        protected readonly StateField field;
+
+        protected StateFieldModifierVariable(string name, LogicManager lm, string fieldName)
+        {
+            Name = name;
+
+            try
+            {
+                field = lm.StateManager.GetBoolStrict(fieldName);
+                //if (field == null)
+                    //field = lm.StateManager.GetIntStrict(fieldName);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException($"Exception while constructing StateFieldModifierVariable ({fieldName})", e);
+            }
+        }
+
+        public static bool TryMatch(LogicManager lm, string term, out LogicVariable variable)
+        {
+            if (fieldModifierNames.TryGetValue(term, out string fieldName))
+            {
+                variable = new StateFieldModifierVariable(term, lm, fieldName);
+                return true;
+            }
+            else
+            {
+                variable = default;
+                return false;
+            }
+        }
+
+        public override IEnumerable<LazyStateBuilder> ModifyState(object? sender, ProgressionManager pm, LazyStateBuilder state)
+        {
+            if (pm.Has(field))
+                yield return state;
+        }
+
+        public override IEnumerable<Term> GetTerms()
+        {
+            return Enumerable.Empty<Term>();
+        }
+    }
+}
